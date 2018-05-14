49efad0a3e4f2feb26ade56d70ea6321b3d315a3
Corentin Henry <corentinhenry@gmail.com>
2018-05-13 17:39:58 -0700

    improve attributes ergonomics
    attributes are pretty arbitrary and parsing depends on the netlink
    message, so we cannot have one type for attributes. Instead, we provide
    an Attribute trait, and implement parsing/emitting for types that
    implement this trait. In addition, we provide a default type for
    attributes, so is just a buffer of bytes.
    
    To make iterating of attributes efficient and pleasant, add an iterator
    that does not actually perform any parsing.

--- src/packet/attribute.rs
+++ src/packet/attribute.rs
@@ -44,7 +44,9 @@ impl<T: AsRef<[u8]>> Packet<T> {
         NativeEndian::read_u16(&data[TYPE])
     }
 
-    /// Return the `length` field
+    /// Return the `length` field. The `length` field corresponds to the length of the attribute
+    /// header (type and length fields, and the value field). However, it does not account for the
+    /// potential padding that follows the value field.
     pub fn length(&self) -> u16 {
         let data = self.buffer.as_ref();
         NativeEndian::read_u16(&data[LENGTH])
@@ -90,47 +92,102 @@ impl<'a, T: AsRef<[u8]> + AsMut<[u8]> + ?Sized> Packet<&'a mut T> {
     }
 }
 
-/// Represent an attribute
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum Attribute {
-    Other { kind: u16, value: Vec<u8> },
+pub struct DefaultAttribute {
+    kind: u16,
+    value: Vec<u8>,
 }
 
-impl Repr for Attribute {
+impl Attribute for DefaultAttribute {
+    fn length(&self) -> usize {
+        self.value.len()
+    }
+    fn kind(&self) -> u16 {
+        self.kind
+    }
+    fn value(&self) -> &[u8] {
+        self.value.as_slice()
+    }
+    fn from_packet<'a, T: AsRef<[u8]> + ?Sized>(packet: Packet<&'a T>) -> Result<Self> {
+        Ok(DefaultAttribute {
+            kind: packet.kind(),
+            value: packet.value()?.to_vec(),
+        })
+    }
+}
+
+pub trait Attribute: Sized {
+    fn length(&self) -> usize;
+    fn kind(&self) -> u16;
+    fn value(&self) -> &[u8];
+    fn from_packet<'a, T: AsRef<[u8]> + ?Sized>(packet: Packet<&'a T>) -> Result<Self>;
+}
+
+impl<T> Repr for T
+where
+    T: Attribute,
+{
     fn parse(buffer: &[u8]) -> Result<Self> {
         let packet = Packet::new_checked(buffer)?;
-        Ok(match packet.kind() {
-            other => Attribute::Other {
-                kind: other,
-                value: packet.value()?.to_vec(),
-            },
-        })
+        T::from_packet(packet)
     }
 
     fn buffer_len(&self) -> usize {
-        use self::Attribute::*;
-        let value_len = match *self {
-            Other { ref value, .. } => value.len(),
-        };
-        value_len + 4
+        self.length() as usize
     }
 
     fn emit(&self, buffer: &mut [u8]) -> Result<()> {
-        use self::Attribute::*;
         if buffer.len() < self.buffer_len() {
             return Err(Error::Exhausted);
         }
         let mut packet = Packet::new(buffer);
-        match *self {
-            Other {
-                ref kind,
-                ref value,
-            } => {
-                packet.set_kind(*kind);
-                packet.set_length(value.len() as u16 + 4);
-                packet.value_mut()?.copy_from_slice(value.as_slice());
+        packet.set_kind(self.kind());
+        packet.set_length(self.length() as u16 + 4);
+        packet.value_mut()?.copy_from_slice(self.value());
+        Ok(())
+    }
+}
+
+pub fn emit_attributes<T, U>(attributes: T)
+where
+    T: Iterator<Item = U>,
+    U: Attribute,
+{
+    unimplemented!()
+}
+
+/// An iterator that iteratates over attributes without decoding them. This is useful when looking
+/// for specific attributes.
+pub struct AttributesIterator<'a> {
+    position: usize,
+    buffer: &'a [u8],
+}
+
+impl<'a> Iterator for AttributesIterator<'a> {
+    type Item = Result<Packet<&'a [u8]>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Attributes are aligned on 4 bytes boundaries, so we make sure we ignore any potential
+        // padding.
+        let offset = self.position % 4;
+        if offset != 0 {
+            self.position += 4 - offset;
+        }
+
+        if self.position >= self.buffer.len() {
+            return None;
+        }
+
+        match Packet::new_checked(&self.buffer[self.position..]) {
+            Ok(packet) => {
+                self.position += packet.length() as usize;
+                return Some(Ok(packet));
+            }
+            Err(e) => {
+                // Make sure next time we call `next()`, we return None. We don't try to continue
+                // iterating after we failed to return a packet.
+                self.position = self.buffer.len();
+                return Some(Err(e));
             }
         }
-        Ok(())
     }
 }
