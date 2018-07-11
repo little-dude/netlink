8cbb213b00a9e799faffb73c9fd83625abe3509a
Corentin Henry <corentinhenry@gmail.com>
2018-07-11 15:17:22 -0700

    more doc

--- iproute2/examples/create_dummy_link.rs
+++ iproute2/examples/create_dummy_link.rs
@@ -10,12 +10,18 @@ use tokio_core::reactor::Core;
 use iproute2::new_connection;
 
 fn main() {
+    // Create a netlink connection, and a handle to send requests via this connection
     let (connection, handle) = new_connection().unwrap();
+
+    // The connection we run in its own thread
     spawn(move || Core::new().unwrap().run(connection));
+
+    // Create a request to create the veth pair
     handle
         .link()
         .add()
-        .dummy("dummy-rs".into())
+        .veth("veth-rs-1".into(), "veth-rs-2".into())
+        // Execute the request, and wait for it to finish
         .execute()
         .wait()
         .unwrap();
--- iproute2/examples/dump_links.rs
+++ iproute2/examples/dump_links.rs
@@ -7,11 +7,19 @@ use iproute2::new_connection;
 use tokio_core::reactor::Core;
 
 fn main() {
-    let mut core = Core::new().unwrap();
+    // Create a netlink connection, and a handle to send requests via this connection
     let (connection, handle) = new_connection().unwrap();
+
+    // The connection will run in an event loop
+    let mut core = Core::new().unwrap();
     core.handle().spawn(connection.map_err(|_| ()));
-    core.run(handle.link().get().execute().and_then(|links| {
+
+    // Create a netlink request
+    let request = handle.link().get().execute().and_then(|links| {
         println!("{:#?}", links);
         Ok(())
-    })).unwrap();
+    });
+
+    // Run the request on the event loop
+    core.run(request).unwrap();
 }
