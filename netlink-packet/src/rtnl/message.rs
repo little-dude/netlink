97843d46e90b787450ff79cdb9e3e011b36b49c4
Gregor Reitzenstein <dequbed@paranoidlabs.org>
2018-08-16 13:51:06 +0200

    Add neighbour and neighbour table messages

--- netlink-packet/src/rtnl/message.rs
+++ netlink-packet/src/rtnl/message.rs
@@ -2,7 +2,8 @@ use crate::constants::*;
 use failure::ResultExt;
 
 use crate::{
-    AddressBuffer, AddressMessage, DecodeError, Emitable, LinkBuffer, LinkMessage, Parseable,
+    AddressBuffer, AddressMessage, DecodeError, Emitable, LinkBuffer, LinkMessage, NeighbourBuffer,
+    NeighbourMessage, NeighbourTableBuffer, NeighbourTableMessage, Parseable,
 };
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -14,6 +15,12 @@ pub enum RtnlMessage {
     NewAddress(AddressMessage),
     DelAddress(AddressMessage),
     GetAddress(AddressMessage),
+    NewNeighbour(NeighbourMessage),
+    GetNeighbour(NeighbourMessage),
+    DelNeighbour(NeighbourMessage),
+    NewNeighbourTable(NeighbourTableMessage),
+    GetNeighbourTable(NeighbourTableMessage),
+    SetNeighbourTable(NeighbourTableMessage),
 }
 
 impl RtnlMessage {
@@ -73,6 +80,54 @@ impl RtnlMessage {
         }
     }
 
+    pub fn is_get_neighbour(&self) -> bool {
+        if let RtnlMessage::GetNeighbour(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_new_neighbour(&self) -> bool {
+        if let RtnlMessage::NewNeighbour(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_del_neighbour(&self) -> bool {
+        if let RtnlMessage::DelNeighbour(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_new_neighbour_table(&self) -> bool {
+        if let RtnlMessage::NewNeighbourTable(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_get_neighbour_table(&self) -> bool {
+        if let RtnlMessage::GetNeighbourTable(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_set_neighbour_table(&self) -> bool {
+        if let RtnlMessage::SetNeighbourTable(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
     pub fn message_type(&self) -> u16 {
         use self::RtnlMessage::*;
 
@@ -84,6 +139,12 @@ impl RtnlMessage {
             NewAddress(_) => RTM_NEWADDR,
             DelAddress(_) => RTM_DELADDR,
             GetAddress(_) => RTM_GETADDR,
+            GetNeighbour(_) => RTM_GETNEIGH,
+            NewNeighbour(_) => RTM_NEWNEIGH,
+            DelNeighbour(_) => RTM_DELNEIGH,
+            GetNeighbourTable(_) => RTM_GETNEIGHTBL,
+            NewNeighbourTable(_) => RTM_NEWNEIGHTBL,
+            SetNeighbourTable(_) => RTM_SETNEIGHTBL,
         }
     }
 
@@ -115,6 +176,30 @@ impl RtnlMessage {
                     _ => unreachable!(),
                 }
             }
+            // Neighbour messages
+            RTM_NEWNEIGH | RTM_GETNEIGH | RTM_DELNEIGH => {
+                let msg: NeighbourMessage = NeighbourBuffer::new(&buffer)
+                    .parse()
+                    .context("invalid neighbour message")?;
+                match message_type {
+                    RTM_GETNEIGH => GetNeighbour(msg),
+                    RTM_NEWNEIGH => NewNeighbour(msg),
+                    RTM_DELNEIGH => DelNeighbour(msg),
+                    _ => unreachable!(),
+                }
+            }
+            // Neighbour table messages
+            RTM_NEWNEIGHTBL | RTM_GETNEIGHTBL | RTM_SETNEIGHTBL => {
+                let msg: NeighbourTableMessage = NeighbourTableBuffer::new(&buffer)
+                    .parse()
+                    .context("invalid neighbour table message")?;
+                match message_type {
+                    RTM_GETNEIGHTBL => GetNeighbourTable(msg),
+                    RTM_NEWNEIGHTBL => NewNeighbourTable(msg),
+                    RTM_SETNEIGHTBL => SetNeighbourTable(msg),
+                    _ => unreachable!(),
+                }
+            }
             _ => return Err(format!("Unknown message type: {}", message_type).into()),
         };
         Ok(message)
@@ -135,7 +220,17 @@ impl Emitable for RtnlMessage {
             | NewAddress(ref msg)
             | DelAddress(ref msg)
             | GetAddress(ref msg)
-            => msg.buffer_len()
+            => msg.buffer_len(),
+
+            | NewNeighbour(ref msg)
+            | GetNeighbour(ref msg)
+            | DelNeighbour(ref msg)
+            => msg.buffer_len(),
+
+            | NewNeighbourTable(ref msg)
+            | GetNeighbourTable(ref msg)
+            | SetNeighbourTable(ref msg)
+            => msg.buffer_len(),
         }
     }
 
@@ -152,7 +247,17 @@ impl Emitable for RtnlMessage {
             | NewAddress(ref msg)
             | DelAddress(ref msg)
             | GetAddress(ref msg)
-            => msg.emit(buffer)
+            => msg.emit(buffer),
+
+            | GetNeighbour(ref msg)
+            | NewNeighbour(ref msg)
+            | DelNeighbour(ref msg)
+            => msg.emit(buffer),
+
+            | GetNeighbourTable(ref msg)
+            | NewNeighbourTable(ref msg)
+            | SetNeighbourTable(ref msg)
+            => msg.emit(buffer),
         }
     }
 }
---
+++ netlink-packet/src/rtnl/neighbour/nlas/cache_info.rs
@@ -0,0 +1,110 @@
+use byteorder::{ByteOrder, NativeEndian};
+
+use crate::{DecodeError, Emitable, Field, Parseable};
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct NeighbourCacheInfo {
+    pub confirmed: u32,
+    pub used: u32,
+    pub updated: u32,
+    pub refcnt: u32,
+}
+
+const CONFIRMED: Field = 0..4;
+const USED: Field = 4..8;
+const UPDATED: Field = 8..12;
+const REFCNT: Field = 12..16;
+pub const NEIGHBOUR_CACHE_INFO_LEN: usize = REFCNT.end;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct NeighbourCacheInfoBuffer<T> {
+    buffer: T,
+}
+
+impl<T: AsRef<[u8]>> NeighbourCacheInfoBuffer<T> {
+    pub fn new(buffer: T) -> NeighbourCacheInfoBuffer<T> {
+        NeighbourCacheInfoBuffer { buffer }
+    }
+
+    pub fn new_checked(buffer: T) -> Result<NeighbourCacheInfoBuffer<T>, DecodeError> {
+        let buf = Self::new(buffer);
+        buf.check_buffer_length()?;
+        Ok(buf)
+    }
+
+    fn check_buffer_length(&self) -> Result<(), DecodeError> {
+        let len = self.buffer.as_ref().len();
+        if len < NEIGHBOUR_CACHE_INFO_LEN {
+            return Err(format!(
+                "invalid NeighbourCacheInfoBuffer buffer: length is {} instead of {}",
+                len, NEIGHBOUR_CACHE_INFO_LEN
+            )
+            .into());
+        }
+        Ok(())
+    }
+
+    pub fn into_inner(self) -> T {
+        self.buffer
+    }
+
+    pub fn confirmed(&self) -> u32 {
+        NativeEndian::read_u32(&self.buffer.as_ref()[CONFIRMED])
+    }
+
+    pub fn used(&self) -> u32 {
+        NativeEndian::read_u32(&self.buffer.as_ref()[USED])
+    }
+
+    pub fn updated(&self) -> u32 {
+        NativeEndian::read_u32(&self.buffer.as_ref()[UPDATED])
+    }
+
+    pub fn refcnt(&self) -> u32 {
+        NativeEndian::read_u32(&self.buffer.as_ref()[REFCNT])
+    }
+}
+
+impl<T: AsRef<[u8]> + AsMut<[u8]>> NeighbourCacheInfoBuffer<T> {
+    pub fn set_confirmed(&mut self, value: u32) {
+        NativeEndian::write_u32(&mut self.buffer.as_mut()[CONFIRMED], value.into())
+    }
+
+    pub fn set_used(&mut self, value: u32) {
+        NativeEndian::write_u32(&mut self.buffer.as_mut()[USED], value.into())
+    }
+
+    pub fn set_updated(&mut self, value: u32) {
+        NativeEndian::write_u32(&mut self.buffer.as_mut()[UPDATED], value.into())
+    }
+
+    pub fn set_refcnt(&mut self, value: u32) {
+        NativeEndian::write_u32(&mut self.buffer.as_mut()[REFCNT], value.into())
+    }
+}
+
+impl<T: AsRef<[u8]>> Parseable<NeighbourCacheInfo> for NeighbourCacheInfoBuffer<T> {
+    fn parse(&self) -> Result<NeighbourCacheInfo, DecodeError> {
+        self.check_buffer_length()?;
+        Ok(NeighbourCacheInfo {
+            confirmed: self.confirmed(),
+            used: self.used(),
+            updated: self.updated(),
+            refcnt: self.refcnt(),
+        })
+    }
+}
+
+impl Emitable for NeighbourCacheInfo {
+    fn buffer_len(&self) -> usize {
+        NEIGHBOUR_CACHE_INFO_LEN
+    }
+
+    fn emit(&self, buffer: &mut [u8]) {
+        let mut buffer = NeighbourCacheInfoBuffer::new(buffer);
+        buffer.set_confirmed(self.confirmed);
+        buffer.set_used(self.used);
+        buffer.set_updated(self.updated);
+        buffer.set_refcnt(self.refcnt);
+    }
+}
