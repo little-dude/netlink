17447427c5b4b33325736d995c18f0f505469b84
Corentin Henry <corentinhenry@gmail.com>
2018-12-29 18:07:22 -0800

    remove public dependencies to ipnetwork and eui48

--- rtnetlink/src/addr/mod.rs
+++ rtnetlink/src/addr/mod.rs
@@ -1,7 +1,3 @@
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-
-use crate::errors::{Error, ErrorKind};
-
 mod handle;
 pub use self::handle::*;
 
@@ -16,19 +12,3 @@ pub use self::get::*;
 
 mod flush;
 pub use self::flush::*;
-
-fn bytes_to_ip_addr(bytes: &[u8]) -> Result<IpAddr, Error> {
-    match bytes.len() {
-        4 => {
-            let mut array = [0; 4];
-            array.copy_from_slice(bytes);
-            Ok(Ipv4Addr::from(array).into())
-        }
-        16 => {
-            let mut array = [0; 16];
-            array.copy_from_slice(bytes);
-            Ok(Ipv6Addr::from(array).into())
-        }
-        _ => Err(ErrorKind::InvalidIp(bytes.to_vec()).into()),
-    }
-}
