716a703196c97be4d2715995b2ffc9b1ce9f5bb1
Gris Ge <cnfourt@gmail.com>
2020-08-02 21:27:26 +0800

    rtnetlink: Add support of querying filters in traffic control
    New function `rtnetlink::Handle::traffic_filter()` with initial traffic
    filter query support.
    
    The `cargo test` will run in parallel, changing loopback interface might
    cause `test_get_qdiscs` fail.
    Hence changed the integration test case to use dummy interface instead
    of loopback interface.
    
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- rtnetlink/src/traffic_control/handle.rs
+++ rtnetlink/src/traffic_control/handle.rs
@@ -1,4 +1,4 @@
-use super::{QDiscGetRequest, TrafficClassGetRequest};
+use super::{QDiscGetRequest, TrafficClassGetRequest, TrafficFilterGetRequest};
 use crate::Handle;
 
 pub struct QDiscHandle(Handle);
@@ -21,11 +21,28 @@ pub struct TrafficClassHandle {
 
 impl TrafficClassHandle {
     pub fn new(handle: Handle, ifindex: i32) -> Self {
-        TrafficClassHandle{handle, ifindex}
+        TrafficClassHandle { handle, ifindex }
     }
 
     /// Retrieve the list of qdisc (equivalent to `tc qdisc show`)
     pub fn get(&mut self) -> TrafficClassGetRequest {
         TrafficClassGetRequest::new(self.handle.clone(), self.ifindex)
+    }
+}
+
+pub struct TrafficFilterHandle {
+    handle: Handle,
+    ifindex: i32,
+}
+
+impl TrafficFilterHandle {
+    pub fn new(handle: Handle, ifindex: i32) -> Self {
+        TrafficFilterHandle { handle, ifindex }
+    }
+
+    /// Retrieve the list of filter (equivalent to
+    /// `tc filter show dev <iface_name>`)
+    pub fn get(&mut self) -> TrafficFilterGetRequest {
+        TrafficFilterGetRequest::new(self.handle.clone(), self.ifindex)
     }
 }
