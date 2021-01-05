e7348f9b7ce7d918473bcd83b17838e90367d23a
gabrik <gabriele.baldoni@gmail.com>
2021-01-05 11:08:27 +0100

    code reorganization and clean up
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- rtnetlink/src/ns.rs
+++ rtnetlink/src/ns.rs
@@ -23,165 +23,13 @@ impl NetworkNamespace {
     /// This is equivalent to `ip netns add NS_NAME`.
     pub async fn add(ns_name: String) -> Result<(), Error> {
         // Forking process to avoid moving caller into new namespace
+        NetworkNamespace::prep_for_fork()?;
         log::trace!("Forking...");
         match unsafe { fork() } {
-            Ok(ForkResult::Parent { child, .. }) => {
-                // as the wait is blocking spawning it in a blocking task
-                let res = task::spawn_blocking(move || {
-                    log::trace!("Parent waiting child: {}", child);
-                    match waitpid(child, None) {
-                        Ok(wait_status) => match wait_status {
-                            WaitStatus::Exited(_, res) => {
-                                log::trace!("Child exist status: {}", res);
-                                if res == 0 {
-                                    return Ok(());
-                                }
-                                let err_msg = format!("Child result: {}", res);
-                                Err(Error::NamespaceError(err_msg))
-                            }
-                            WaitStatus::Signaled(_, signal, has_dump) => {
-                                let err_msg = format!(
-                                    "Child process was killed by signal: {} with core dump {}",
-                                    signal, has_dump
-                                );
-                                Err(Error::NamespaceError(err_msg))
-                            }
-                            _ => {
-                                let err_msg = String::from("Unknown child process status");
-                                Err(Error::NamespaceError(err_msg))
-                            }
-                        },
-                        Err(e) => {
-                            let err_msg = format!("wait failed: {}", e);
-                            Err(Error::NamespaceError(err_msg))
-                        }
-                    }
-                })
-                .await;
-                match res {
-                    Ok(r) => r,
-                    Err(e) => {
-                        let err_msg = format!("wait failed: {}", e);
-                        log::error!("{}", err_msg);
-                        Err(Error::NamespaceError(err_msg))
-                    }
-                }
-            }
+            Ok(ForkResult::Parent { child, .. }) => NetworkNamespace::parent_process(child),
             Ok(ForkResult::Child) => {
-                log::trace!("Child creating namespace");
-
-                let mut netns_path = String::new();
-
-                let dir_path = Path::new(NETNS_PATH);
-                let mut mkdir_mode = Mode::empty();
-                let mut open_flags = OFlag::empty();
-                let mut mount_flags = nix::mount::MsFlags::empty();
-                let mut setns_flags = CloneFlags::empty();
-                let none_fs = Path::new(&NONE_FS);
-                let none_p4: Option<&Path> = None;
-
-                // flags in mkdir
-                mkdir_mode.insert(Mode::S_IRWXU);
-                mkdir_mode.insert(Mode::S_IRGRP);
-                mkdir_mode.insert(Mode::S_IXGRP);
-                mkdir_mode.insert(Mode::S_IROTH);
-                mkdir_mode.insert(Mode::S_IXOTH);
-
-                open_flags.insert(OFlag::O_RDONLY);
-                open_flags.insert(OFlag::O_CREAT);
-                open_flags.insert(OFlag::O_EXCL);
-
-                netns_path.push_str(NETNS_PATH);
-                netns_path.push_str(&ns_name);
-
-                // creating namespaces folder if not exists
-                #[allow(clippy::collapsible_if)]
-                if nix::sys::stat::stat(dir_path).is_err() {
-                    if nix::unistd::mkdir(dir_path, mkdir_mode).is_err() {
-                        exit(-1);
-                    }
-                }
-
-                mount_flags.insert(nix::mount::MsFlags::MS_BIND);
-                mount_flags.insert(nix::mount::MsFlags::MS_REC);
-
-                if nix::mount::mount(
-                    Some(Path::new(dir_path)),
-                    dir_path,
-                    Some(none_fs),
-                    mount_flags,
-                    none_p4,
-                )
-                .is_err()
-                {
-                    exit(-1);
-                }
-
-                mount_flags = nix::mount::MsFlags::empty();
-                mount_flags.insert(nix::mount::MsFlags::MS_SHARED);
-                mount_flags.insert(nix::mount::MsFlags::MS_REC);
-                if nix::mount::mount(
-                    Some(Path::new("")),
-                    dir_path,
-                    Some(none_fs),
-                    mount_flags,
-                    none_p4,
-                )
-                .is_err()
-                {
-                    exit(-1);
-                }
-
-                let ns_path = Path::new(&netns_path);
-
-                // creating the netns file
-                let fd = match nix::fcntl::open(ns_path, open_flags, Mode::empty()) {
-                    Ok(raw_fd) => raw_fd,
-                    Err(_) => exit(-1),
-                };
-
-                if nix::unistd::close(fd).is_err() {
-                    let _ = nix::unistd::unlink(ns_path);
-                    exit(-1)
-                }
-
-                // unshare to the new network namespace
-                if nix::sched::unshare(CloneFlags::CLONE_NEWNET).is_err() {
-                    let _ = nix::unistd::unlink(ns_path);
-                    exit(-1);
-                }
-                open_flags = OFlag::empty();
-                open_flags.insert(OFlag::O_RDONLY);
-                open_flags.insert(OFlag::O_CLOEXEC);
-
-                let fd = match nix::fcntl::open(Path::new(&SELF_NS_PATH), open_flags, Mode::empty())
-                {
-                    Ok(raw_fd) => raw_fd,
-                    Err(_) => exit(-1),
-                };
-
-                let self_path = Path::new(&SELF_NS_PATH);
-
-                // bind to the netns
-                if nix::mount::mount(
-                    Some(self_path),
-                    ns_path,
-                    Some(none_fs),
-                    nix::mount::MsFlags::MS_BIND,
-                    none_p4,
-                )
-                .is_err()
-                {
-                    let _ = nix::unistd::unlink(ns_path);
-                    exit(-1);
-                }
-
-                setns_flags.insert(CloneFlags::CLONE_NEWNET);
-                if nix::sched::setns(fd, setns_flags).is_err() {
-                    let _ = nix::unistd::unlink(ns_path);
-                    exit(-1);
-                }
-
+                let netns_path = NetworkNamespace::child_process(ns_name)?;
+                NetworkNamespace::unshare_processing(netns_path)?;
                 exit(0)
             }
             Err(e) => {
@@ -201,7 +49,7 @@ impl NetworkNamespace {
             let ns_path = Path::new(&netns_path);
 
             if nix::mount::umount2(ns_path, nix::mount::MntFlags::MNT_DETACH).is_err() {
-                let err_msg = String::from("Namespace unmound failed (are you running as root?)");
+                let err_msg = String::from("Namespace unmount failed (are you running as root?)");
                 return Err(Error::NamespaceError(err_msg));
             }
 
@@ -219,5 +67,235 @@ impl NetworkNamespace {
                 Err(Error::NamespaceError(err_msg))
             }
         }
+    }
+
+    pub fn prep_for_fork() -> Result<(), Error> {
+        // Placeholder function, nothing to do here.
+        Ok(())
+    }
+
+    /// This is the parent process form the fork, it waits for the
+    /// child to exit properly
+    pub fn parent_process(child: nix::unistd::Pid) -> Result<(), Error> {
+        log::trace!("parent_process child PID: {}", child);
+        log::trace!("Waiting for child to finish...");
+        match waitpid(child, None) {
+            Ok(wait_status) => match wait_status {
+                WaitStatus::Exited(_, res) => {
+                    log::trace!("Child exited with: {}", res);
+                    if res == 0 {
+                        return Ok(());
+                    }
+                    log::error!("Error child result: {}", res);
+                    let err_msg = format!("Error child result: {}", res);
+                    Err(Error::NamespaceError(err_msg))
+                }
+                WaitStatus::Signaled(_, signal, has_dump) => {
+                    log::error!("Error child killed by signal: {}", signal);
+                    let err_msg = format!(
+                        "Error child process was killed by signal: {} with core dump {}",
+                        signal, has_dump
+                    );
+                    Err(Error::NamespaceError(err_msg))
+                }
+                _ => {
+                    log::error!("Unknown child process status");
+                    let err_msg = String::from("Unknown child process status");
+                    Err(Error::NamespaceError(err_msg))
+                }
+            },
+            Err(e) => {
+                log::error!("wait error: {}", e);
+                let err_msg = format!("wait error: {}", e);
+                Err(Error::NamespaceError(err_msg))
+            }
+        }
+    }
+
+    /// This is the child process, it will actually create the namespace
+    /// resources. It creates the folder and namespace file.
+    /// Returns the namespace file path
+    pub fn child_process(ns_name: String) -> Result<String, Error> {
+        log::trace!("child_process will create the namespace");
+
+        let mut netns_path = String::new();
+
+        let dir_path = Path::new(NETNS_PATH);
+        let mut mkdir_mode = Mode::empty();
+        let mut open_flags = OFlag::empty();
+        let mut mount_flags = nix::mount::MsFlags::empty();
+        let none_fs = Path::new(&NONE_FS);
+        let none_p4: Option<&Path> = None;
+
+        // flags in mkdir
+        mkdir_mode.insert(Mode::S_IRWXU);
+        mkdir_mode.insert(Mode::S_IRGRP);
+        mkdir_mode.insert(Mode::S_IXGRP);
+        mkdir_mode.insert(Mode::S_IROTH);
+        mkdir_mode.insert(Mode::S_IXOTH);
+
+        open_flags.insert(OFlag::O_RDONLY);
+        open_flags.insert(OFlag::O_CREAT);
+        open_flags.insert(OFlag::O_EXCL);
+
+        netns_path.push_str(NETNS_PATH);
+        netns_path.push_str(&ns_name);
+
+        // creating namespaces folder if not exists
+        #[allow(clippy::collapsible_if)]
+        if nix::sys::stat::stat(dir_path).is_err() {
+            match nix::unistd::mkdir(dir_path, mkdir_mode) {
+                Err(e) => {
+                    log::error!("mkdir error: {}", e);
+                    let err_msg = format!("mkdir error: {}", e);
+                    return Err(Error::NamespaceError(err_msg));
+                }
+                Ok(_) => (),
+            }
+        }
+
+        // Check try to mount /run/netns, with MS_REC | MS_SHARED
+        // if fails creates the mount with MS_BIND | MS_REC
+        // this is the same strategy used by `ip netns add NS`
+        mount_flags.insert(nix::mount::MsFlags::MS_REC);
+        mount_flags.insert(nix::mount::MsFlags::MS_SHARED);
+        if nix::mount::mount(
+            Some(Path::new("")),
+            dir_path,
+            Some(none_fs),
+            mount_flags,
+            none_p4,
+        )
+        .is_err()
+        {
+            mount_flags = nix::mount::MsFlags::empty();
+            mount_flags.insert(nix::mount::MsFlags::MS_BIND);
+            mount_flags.insert(nix::mount::MsFlags::MS_REC);
+
+            match nix::mount::mount(
+                Some(Path::new(dir_path)),
+                dir_path,
+                Some(none_fs),
+                mount_flags,
+                none_p4,
+            ) {
+                Err(e) => {
+                    log::error!("mount error: {}", e);
+                    let err_msg = format!("mount error: {}", e);
+                    return Err(Error::NamespaceError(err_msg));
+                }
+                Ok(_) => (),
+            }
+        }
+
+        mount_flags = nix::mount::MsFlags::empty();
+        mount_flags.insert(nix::mount::MsFlags::MS_REC);
+        mount_flags.insert(nix::mount::MsFlags::MS_SHARED);
+        match nix::mount::mount(
+            Some(Path::new("")),
+            dir_path,
+            Some(none_fs),
+            mount_flags,
+            none_p4,
+        ) {
+            Err(e) => {
+                log::error!("mount error: {}", e);
+                let err_msg = format!("mount error: {}", e);
+                return Err(Error::NamespaceError(err_msg));
+            }
+            Ok(_) => (),
+        }
+
+        let ns_path = Path::new(&netns_path);
+
+        // creating the netns file
+        let fd = match nix::fcntl::open(ns_path, open_flags, Mode::empty()) {
+            Ok(raw_fd) => raw_fd,
+            Err(e) => {
+                log::error!("open error: {}", e);
+                let err_msg = format!("open error: {}", e);
+                return Err(Error::NamespaceError(err_msg));
+            }
+        };
+
+        match nix::unistd::close(fd) {
+            Err(e) => {
+                log::error!("close error: {}", e);
+                let err_msg = format!("close error: {}", e);
+                let _ = nix::unistd::unlink(ns_path);
+                return Err(Error::NamespaceError(err_msg));
+            }
+            Ok(_) => (),
+        }
+
+        Ok(netns_path)
+    }
+
+    /// This function unshare the calling process and move into
+    /// the given network namespace
+    #[allow(unused)]
+    pub fn unshare_processing(netns_path: String) -> Result<(), Error> {
+        let mut setns_flags = CloneFlags::empty();
+        let mut open_flags = OFlag::empty();
+        let ns_path = Path::new(&netns_path);
+
+        let none_fs = Path::new(&NONE_FS);
+        let none_p4: Option<&Path> = None;
+
+        // unshare to the new network namespace
+        match nix::sched::unshare(CloneFlags::CLONE_NEWNET) {
+            Err(e) => {
+                log::error!("unshare error: {}", e);
+                let err_msg = format!("unshare error: {}", e);
+                let _ = nix::unistd::unlink(ns_path);
+                return Err(Error::NamespaceError(err_msg));
+            }
+            Ok(_) => (),
+        }
+
+        open_flags = OFlag::empty();
+        open_flags.insert(OFlag::O_RDONLY);
+        open_flags.insert(OFlag::O_CLOEXEC);
+
+        let fd = match nix::fcntl::open(Path::new(&SELF_NS_PATH), open_flags, Mode::empty()) {
+            Ok(raw_fd) => raw_fd,
+            Err(e) => {
+                log::error!("open error: {}", e);
+                let err_msg = format!("open error: {}", e);
+                return Err(Error::NamespaceError(err_msg));
+            }
+        };
+
+        let self_path = Path::new(&SELF_NS_PATH);
+
+        // bind to the netns
+        match nix::mount::mount(
+            Some(self_path),
+            ns_path,
+            Some(none_fs),
+            nix::mount::MsFlags::MS_BIND,
+            none_p4,
+        ) {
+            Err(e) => {
+                log::error!("mount error: {}", e);
+                let err_msg = format!("mount error: {}", e);
+                let _ = nix::unistd::unlink(ns_path);
+                return Err(Error::NamespaceError(err_msg));
+            }
+            Ok(_) => (),
+        }
+
+        setns_flags.insert(CloneFlags::CLONE_NEWNET);
+        match nix::sched::setns(fd, setns_flags) {
+            Err(e) => {
+                log::error!("setns error: {}", e);
+                let err_msg = format!("setns error: {}", e);
+                let _ = nix::unistd::unlink(ns_path);
+                return Err(Error::NamespaceError(err_msg));
+            }
+            Ok(_) => (),
+        }
+
+        Ok(())
     }
 }
