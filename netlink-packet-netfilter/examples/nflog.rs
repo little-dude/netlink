16844a86c545555f83adf97bb68ed23096511a47
Loïc Damien <loic.damien@dzamlo.ch>
2021-11-24 17:47:20 +0100

    Improve nflog example

--- netlink-packet-netfilter/examples/nflog.rs
+++ netlink-packet-netfilter/examples/nflog.rs
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: MIT
 
+// To run this example:
+//   1) create a iptables/nft rules that send packet with group 1, for example:
+//          sudo iptables -A INPUT -j NFLOG --nflog-group 1
+//   2) build the example:
+//          cargo build --example nflog
+//   3) run it as root:
+//          sudo ../target/debug/examples/nflog
+
+use std::time::Duration;
+
 use netlink_packet_netfilter::{
     constants::*,
     message::NetfilterMessage,
     nflog::{
         self,
-        config::{ConfigCmd, ConfigFlags, ConfigMode},
+        config::{ConfigCmd, ConfigFlags, ConfigMode, Timeout},
     },
     NetlinkMessage,
     NetlinkPayload,
@@ -13,16 +23,28 @@ use netlink_packet_netfilter::{
 use netlink_sys::{constants::NETLINK_NETFILTER, Socket};
 
 fn main() {
+    let mut receive_buffer = vec![0; 4096];
+
+    // First, we bind the socket
     let mut socket = Socket::new(NETLINK_NETFILTER).unwrap();
     socket.bind_auto().unwrap();
 
+    // Then we issue the PfBind command
     let packet = nflog::config::config_request(AF_INET, 0, vec![ConfigCmd::PfBind.into()]);
     let mut buf = vec![0; packet.header.length as usize];
     packet.serialize(&mut buf[..]);
     println!(">>> {:?}", packet);
     socket.send(&buf[..], 0).unwrap();
-    // TODO: check the response
 
+    // And check there is no error
+    let size = socket.recv(&mut &mut receive_buffer[..], 0).unwrap();
+    let bytes = &receive_buffer[..size];
+    let rx_packet = <NetlinkMessage<NetfilterMessage>>::deserialize(bytes).unwrap();
+    println!("<<< {:?}", rx_packet);
+    assert!(matches!(rx_packet.payload, NetlinkPayload::Ack(_)));
+
+    // After that we issue a Bind command, to start receiving packets. We can also set various parameters at the same time
+    let timeout: Timeout = Duration::from_millis(100).into();
     let packet = nflog::config::config_request(
         AF_INET,
         1,
@@ -30,17 +52,23 @@ fn main() {
             ConfigCmd::Bind.into(),
             ConfigFlags::SEQ_GLOBAL.into(),
             ConfigMode::new_packet(16).into(),
+            timeout.into(),
         ],
     );
     let mut buf = vec![0; packet.header.length as usize];
     packet.serialize(&mut buf[..]);
     println!(">>> {:?}", packet);
     socket.send(&buf[..], 0).unwrap();
-    // TODO: check the response
 
-    let mut receive_buffer = vec![0; 4096];
+    let size = socket.recv(&mut &mut receive_buffer[..], 0).unwrap();
+    let bytes = &receive_buffer[..size];
+    let rx_packet = <NetlinkMessage<NetfilterMessage>>::deserialize(bytes).unwrap();
+    println!("<<< {:?}", rx_packet);
+    assert!(matches!(rx_packet.payload, NetlinkPayload::Ack(_)));
+
+    // And now we can receive the packets
+
     let mut offset = 0;
-
     while let Ok(size) = socket.recv(&mut &mut receive_buffer[..], 0) {
         loop {
             let bytes = &receive_buffer[offset..];
